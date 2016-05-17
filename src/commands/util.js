1bcee794e343212c91a03cadeebf50734b8a634d
ANDREW M. TRICE <amtrice@us.ibm.com>
2016-05-17 12:35:03 -0400

    restructuring source/builds/github content organization

---
+++ src/commands/util.js
@@ -0,0 +1,64 @@
+"use strict";
+
+var vscode = require('vscode');
+let log;
+
+function pad(input, length) {
+	if (input == undefined) {
+		input = "";
+	}
+	while (input.length < length) {
+		input += " ";
+	}
+	return input;
+}
+
+function appendHeading(name) {
+	log.appendLine("\n"+name+"\n---------------------------------------------------------------------------------");
+}
+
+function appendEntry(entry, qualified) {
+		
+	var qualifiedName = formatQualifiedName(entry, qualified);
+    var suffix = ""
+	
+	if ( entry.hasOwnProperty("binding") && entry.binding ) {
+		suffix = " binding";
+	}
+    
+	log.appendLine( pad(qualifiedName, 66) + (entry.publish ? "public":"private") + suffix);	
+}
+
+function formatQualifiedName(entry, qualified) {
+    if (qualified == undefined) {
+        qualified = true;
+    }
+    
+    var qualifiedName = (qualified ? (entry.namespace + "/"):"") + entry.name;
+	return qualifiedName;
+}
+
+function setLog(_log) {
+    log = _log;
+}
+
+function printOpenWhiskError(error) {
+    log.appendLine("\nERROR: "+error.toString());
+    if (error.error.activationId) {
+        log.appendLine("activationId: "+error.error.activationId);
+    }
+    if (error.error.logs && (error.error.logs.length > 0)) {
+        for (var x=0; x<error.error.logs.length; x++) {
+            log.appendLine(error.error.logs[x]);
+        }
+    }
+}
+
+module.exports = {
+    pad:pad,
+    appendHeading:appendHeading,
+    appendEntry:appendEntry,
+    formatQualifiedName:formatQualifiedName,
+    setLog:setLog,
+    printOpenWhiskError: printOpenWhiskError
+}
---
+++ src/commands/wsk.rule.js
@@ -0,0 +1,54 @@
+"use strict";
+
+var vscode = require('vscode');
+let util = require("./util.js");
+
+var log;
+var ow;
+var props;
+
+function register(_ow, context, _log, _props) {
+    ow = _ow;
+    log = _log;
+    props = _props;
+    
+    var defaultDisposable = vscode.commands.registerCommand('extension.wsk.rule', defaultAction);
+    context.subscriptions.push(defaultDisposable);
+}
+
+
+function defaultAction(params) {
+
+    log.show(true);
+    log.appendLine("\n$ wsk rule");
+    log.appendLine("available commands:");
+    log.appendLine("    create              create new rule");
+    log.appendLine("    delete              delete rule");
+    log.appendLine("    update              update an existing rule");
+    log.appendLine("    enable              enable rule");
+    log.appendLine("    disable             disable rule");
+    log.appendLine("    status              get rule status");
+    log.appendLine("    get                 get rule");
+    log.appendLine("    list                list all rules");
+}
+
+function list() {
+    
+    if (!props.validate()){
+        return;
+    }
+    
+    return ow.rules.list().then(function (rules) {
+        util.appendHeading("rules");
+        for (var x=0; x<rules.length; x ++){
+                util.appendEntry(rules[x]);	
+        }
+    }).catch(function(error) {
+        log.appendLine(error.toString())
+    });
+}
+
+module.exports = {
+  register: register,
+  list:list
+};
