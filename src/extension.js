8d1939be6e2c7c237b0864637ca3d7b6ec53f2fd
Jeff Sloyer <jsloyer@gmail.com>
2016-05-24 13:51:25 -0400

    fix styling

--- src/extension.js
+++ src/extension.js
@@ -12,13 +12,13 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-openwhisk" is now active!'); 
+	console.log('Congratulations, your extension "vscode-openwhisk" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	
-    controller.init(context);
+
+	controller.init(context);
 }
 exports.activate = activate;
 
