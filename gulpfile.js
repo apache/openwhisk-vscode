cf685c9c4449814864ef361019414f03dccce0d3
Jeff Sloyer <jsloyer@gmail.com>
2016-08-04 22:08:07 -0400

    builds zip file

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var zip = require('gulp-vinyl-zip').zip;
+require('./build/gulpfile.hygiene');
+
+gulp.task('build-zip', () => {
+	const platform = process.platform;
+
+	if (platform !== 'darwin' || platform !== 'win32') {
+		//TODO logic needed here for linux
+	}
+
+	return gulp.src([
+		'out/commands/**',
+		'out/templates/**',
+		'out/extension.js',
+		'node_modules/**',
+		'LICENSE',
+		'package.json',
+		'github-assets/**',
+		'README.md'
+	], { base: '.' })
+		.pipe(zip(`openwhisk-vscode-${platform}.zip`))
+		.pipe(gulp.dest('./out'));
+});
